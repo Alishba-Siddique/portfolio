b5fc0b6225ebbdc7fc1ac929437ee5463d8d33ab
Alishba Siddique <97811058+Alishba-Siddique@users.noreply.github.com>
2025-08-22 02:54:06 +0500

    fixed loader state

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 // src/app/page.js
 'use client';
 import styles from './page.module.scss';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { AnimatePresence } from 'framer-motion';
 import Preloader from '../components/Preloader/PreLoader';
 import Landing from '../components/Landing/Landing';
@@ -16,8 +16,10 @@ export default function Home() {
   const dispatch = useDispatch();
   const isLoading = useSelector((state) => state.ui.isLoading);
   const scrollRef = useRef(null);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
+    setIsMounted(true);
     (async () => {
       const LocomotiveScroll = (await import('locomotive-scroll')).default;
       scrollRef.current = new LocomotiveScroll();
@@ -39,7 +41,7 @@ export default function Home() {
   return (
     <main className={styles.main} style={{ overflowX: 'hidden' }}>
       <AnimatePresence mode="wait">
-        {isLoading && <Preloader />}
+        {isLoading && isMounted && <Preloader />}
       </AnimatePresence>
       <Landing />
       <Description />
